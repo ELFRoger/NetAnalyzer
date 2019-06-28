269703fff42bbfac26971fbc476e7a283b28a491
4everdestiny <347294329@qq.com>
2019-06-28 17:19:26 +0800

    [modify] change config info

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ DB_CONFIG = {
     'DB_HOST': '127.0.0.1',
     'DB_PORT': '3306',
     'DB_USER': 'root',
-    'DB_PASS': 'admin',
-    'DB_DATABASE': 'NetAnalyze',
+    'DB_PASS': 'yuge',
+    'DB_CHARSET': 'utf8',
+    'DB_DATABASE': 'Net_Analyzer',
     'DB_FILE': './db.sqlite3'
 }
