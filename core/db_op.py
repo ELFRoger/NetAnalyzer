b0951011c93db525d0e25f56f71859e98db2c412
4everdestiny <347294329@qq.com>
2019-07-11 21:51:46 +0800

    [modify] add select ua

--- core/db_op.py
+++ core/db_op.py
@@ -1,4 +1,5 @@
 import libs.common
+import libs.db
 import json
 
 def update_originalData(http_list):
@@ -8,7 +9,9 @@ def update_originalData(http_list):
     data_count = len(http_list)
     count = 0
     success = 0
+    param = []
     for http_data in http_list:
+        '''
         sql_statement = ('INSERT INTO http_raw_data'
                         '(`time`, `src`, `dst`, `src_port`, `dst_port`, `http_type`, `method`, `url`, '
                         '`http_version`, `status`, `reason`, `headers`, `body` )'
@@ -16,7 +19,11 @@ def update_originalData(http_list):
                         '%s AS f8, %s AS f9, %s AS f10, %s AS f11, %s AS f12, %s AS f13 '
                         'WHERE NOT EXISTS'
                         '(SELECT * FROM http_raw_data WHERE time=%s AND src=%s AND dst=%s LIMIT 1)')
-
+        '''
+        sql_statement = ('INSERT INTO http_raw_data'
+                        '(`time`, `src`, `dst`, `src_port`, `dst_port`, `http_type`, `method`, `url`, '
+                        '`http_version`, `status`, `reason`, `headers`, `body` )'
+                        'VALUE (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)')
         time = http_data['time']
         src = http_data['src']
         dst = http_data['dst']
@@ -36,16 +43,68 @@ def update_originalData(http_list):
         else:
             body = tmp_data
 
-        db_cursor.execute(sql_statement, (
+        param.append((
             time, src, dst, src_port, dst_port, http_type, method, url, http_version, status, reason, headers, body,
-            time, src, dst,
         ))
         count += 1
+        success += 1
         # every 1000 data commit once
-        if count == 100 or (data_count - success) / 100 - count == 0:
+        if count == 1000 or data_count == success:
+            db_cursor.executemany(sql_statement, param)
             db_conn.commit()
-            success += count
             count = 0
+            param = []
             libs.logger.log('[%s / %s] insert db successfully' %(success, data_count))
     return
 
+def request_filter():
+    db_cursor = libs.common.get_value('db_cursor')
+    db_conn = libs.common.get_value('db_conn')
+
+    sql_statement = ('insert into http_request (time,src,src_port,method,url,http_version,headers) '
+                     'select time,src,src_port,method,url,http_version,headers '
+                     'from http_raw_data where http_type="request"')
+    db_cursor.execute(sql_statement)
+    db_conn.commit()
+    return
+
+def response_filter():
+    db_cursor = libs.common.get_value('db_cursor')
+    db_conn = libs.common.get_value('db_conn')
+
+    sql_statement = ('insert into '
+                     'http_response(time, src, src_port, http_version, status, reason, headers)'
+                     'select time, src, src_port, http_version, status, reason, headers'
+                     'from http_raw_data where http_type = "response"')
+    db_cursor.execute(sql_statement)
+    db_conn.commit()
+    return
+
+def select_all_UA():
+
+    db_cursor = libs.common.get_value('db_cursor')
+    db_conn = libs.common.get_value('db_conn')
+
+    sql_statement = ("select src, headers from http_request where headers!='{}'")
+
+    db_cursor.execute(sql_statement)
+    db_conn.commit()
+    res = db_cursor.fetchall()
+    UA_list = []
+    for item in res:
+        header = json.loads(item[1])
+        if 'user-agent' in header.keys():
+            temp = {}
+            temp['ip'] = item[0]
+            temp['UA'] = header['user-agent']
+            UA_list.append(temp)
+        else:
+            continue
+
+    return UA_list
+
+if __name__ == '__main__':
+    libs.common._init()
+    libs.db.db_cursor_init()
+
+    print(select_all_UA())
