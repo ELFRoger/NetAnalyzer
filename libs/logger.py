fa691c2c10c7935049f45c20f2cecb248021eddb
ELFRoger <30763960+ELFRoger@users.noreply.github.com>
2019-11-10 17:34:54 +0800

    log to logfile

--- libs/logger.py
+++ libs/logger.py
@@ -10,4 +10,7 @@ def log(*message):
 def log(*message,end='\n'):
     time_str = str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     msg_str = ' '.join((str(i) for i in message))
-    print('[%s] %s' % (time_str, msg_str), flush=True, end=end)
+    #print('[%s] %s' % (time_str, msg_str), flush=True, end=end)
+    logname = 'worklog' + time_str.split(' ')[0] + '.log'
+    with open('logname','a+') as logfile:
+        logfile.write('[%s] %s' % (time_str, msg_str))
